6e232ec050c36fc08dfcbe8e44ccc3f2cb709f5c
Classement30 <classement30@gmail.com>
2025-11-20 16:05:03 +0100

    Ajout des nouveaux fichiers

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Ambassadeur/',
+  base: "/Ambassadeur/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
